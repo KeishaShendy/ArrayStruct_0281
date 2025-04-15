4c60425580ce491d0a04f4cae2f45791ee6b5601
keishashendy <shendyarrafli7@gmail.com>
2025-04-15 11:40:55 +0700

    menampilkan data struct

--- struct2.cpp
+++ struct2.cpp
@@ -14,3 +14,23 @@ struct mahasiswa
     string alamat;
 };
 
+int main(){
+    mahasiswa mhs;
+
+    mhs.nim = "20220140102";
+    mhs.nama = "Abra Yudhistira";
+    mhs.alamat = "Kasihan";
+
+    cout << "Masukkan NIM = ";
+    cin >> mhs.nim;
+    cout << "Masukkan nama ";
+    cin >> mhs.nama;
+    cout << "Masukkan alamat ";
+    cin >> mhs.alamat;
+
+    //menampilkan data struct
+    cout << endl;
+    cout << "NIM = " << mhs.nim << endl;
+    cout << "Nama = " << mhs.nama << endl;
+    cout << "Alamat = " << mhs.alamat << endl;
+}
